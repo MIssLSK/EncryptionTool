8a635e30c6e22150b456571b826b47d258ee6b2a
锟 <2820241167@qq.com>
2025-12-16 15:35:32 +0800

    Delete EncryptionAlgorithm.java

--- EncryptionAlgorithm.java
+++
@@ -1,9 +0,0 @@
-package com.encryptiontool.algorithms;
-
-public interface EncryptionAlgorithm {
-    String encrypt(String data, String key) throws Exception;
-    String decrypt(String encryptedData, String key) throws Exception;
-    String getAlgorithmName();
-    String generateKey() throws Exception;
-    boolean requiresKey();
-}
